eaefd7a09887b9a62cd010df05203a135e23bea7
António Wu <81578829+30001546@users.noreply.github.com>
2021-12-15 15:32:23 +0000

    v2.0
    optimized imports

--- Ex - AC3.py
+++ Ex - AC3.py
@@ -1,26 +1,16 @@
-import numpy as np
 import queue
 
 # Matrix visualized
-#                 1  2  3  4  5  6  7  8  9
-line1 = np.array([0, 0, 3, 0, 2, 0, 6, 0, 0])  # A
-line2 = np.array([9, 0, 0, 3, 0, 5, 0, 0, 1])  # B
-line3 = np.array([0, 0, 1, 8, 0, 6, 4, 0, 0])  # C
-line4 = np.array([0, 0, 8, 1, 0, 2, 9, 0, 0])  # D
-line5 = np.array([7, 0, 0, 0, 0, 0, 0, 0, 8])  # E
-line6 = np.array([0, 0, 6, 7, 0, 8, 2, 0, 0])  # F
-line7 = np.array([0, 0, 2, 6, 0, 9, 5, 0, 0])  # G
-line8 = np.array([8, 0, 0, 2, 0, 3, 0, 0, 9])  # H
-line9 = np.array([0, 0, 5, 0, 1, 0, 3, 0, 0])  # I
-matrix = np.array([line1,
-                   line2,
-                   line3,
-                   line4,
-                   line5,
-                   line6,
-                   line7,
-                   line8,
-                   line9])
+#          1  2  3  4  5  6  7  8  9
+matrix = [[0, 0, 3, 0, 2, 0, 6, 0, 0],  # A
+          [9, 0, 0, 3, 0, 5, 0, 0, 1],  # B
+          [0, 0, 1, 8, 0, 6, 4, 0, 0],  # C
+          [0, 0, 8, 1, 0, 2, 9, 0, 0],  # D
+          [7, 0, 0, 0, 0, 0, 0, 0, 8],  # E
+          [0, 0, 6, 7, 0, 8, 2, 0, 0],  # F
+          [0, 0, 2, 6, 0, 9, 5, 0, 0],  # G
+          [8, 0, 0, 2, 0, 3, 0, 0, 9],  # H
+          [0, 0, 5, 0, 1, 0, 3, 0, 0]]  # I
 
 
 # Method to fill the domain of each cell from the Sudoku
